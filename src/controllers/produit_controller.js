2692b4c7a195812bb993ae854084a1661c45baba
Hamza <h.moumine@outlook.com>
2026-06-15 15:16:53 +0200

    Ajout du controlleur de produits

---
+++ src/controllers/produit_controller.js
@@ -0,0 +1,28 @@
+const productService = require("../services/productService");
+
+exports.create = (req, res) => {
+  try {
+    const product = productService.create(
+      req.body.name,
+      req.body.price,
+      req.body.stockQuantity
+    );
+    res.json(product);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+};
+
+exports.update = (req, res) => {
+  try {
+    const product = productService.update(req.params.id, req.body);
+    res.json(product);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+};
+
+exports.delete = (req, res) => {
+  productService.delete(req.params.id);
+  res.json({ message: "deleted" });
+};
