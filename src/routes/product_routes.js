c1fa5f501fcb089b5eaba10ada9cd309e232f69e
Hamza <h.moumine@outlook.com>
2026-06-15 15:18:26 +0200

    Update produit routes

--- src/routes/product_routes.js
+++ src/routes/product_routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/productController");
+const controller = require("../controllers/produit_controller");
 
 router.post("/", controller.create);
 router.put("/:id", controller.update);
